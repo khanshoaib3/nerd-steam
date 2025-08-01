b5042e7fe2f5e683d5d84f83378633fe9c8a97d9
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2025-08-02 03:27:19 +0530

    Updated deps

--- app/src/main/java/com/github/khanshoaib3/steamcompanion/ui/screen/home/components/SteamChartsTableBody.kt
+++ app/src/main/java/com/github/khanshoaib3/steamcompanion/ui/screen/home/components/SteamChartsTableBody.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LoadingIndicator
 import androidx.compose.runtime.Composable
