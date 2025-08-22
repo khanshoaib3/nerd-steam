5c2a0fb623312383b1ded6ff67818578bffc8a02
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2025-08-23 03:18:38 +0530

    Remove -release suffix from release config.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ android {
             )
             isShrinkResources = true
             isDebuggable = false
-            versionNameSuffix = "-release"
         }
         debug {
             isDebuggable = true
